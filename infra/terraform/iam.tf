portable-genai/consumer-duty-monitoring

Make customer-managed encryption optional, and off by default
Every binding to the stack's Cloud KMS key now sits behind cmek_enabled,
default false: the key ring, the key, every grant to a service agent, and
every store, log bucket, registry and revision that took the key. Block
bindings became dynamic blocks; scalar bindings read the key through one(),
which is null when the key does not exist and omits the attribute rather
than pointing it at nothing. The Terraform tests that assert on key material
turn the gate on to keep meaning what they meant.

Why the default is off: a key ring can never be deleted, a log bucket that
has CMEK can never drop it, and registries and document stores take their
key at creation. None of that changes an answer or a screen, and everything
is encrypted at rest with Google-managed keys regardless. A deployment with
a customer whose data it must shred, whose key access must be audited or
whose keys must live in an HSM sets it true in its own tfvars BEFORE the
first apply. Where a stack already applied it, the key's prevent_destroy
refuses a plan that omits the line, which is the right answer: the stores it
bound stay bound.

The vendored deploy-and-residency-hardening skill says the same.

infra/terraform/iam.tf
@@ -50,7 +50,8 @@ resource "google_project_iam_member" "app" {
 
 # The app uses the CMEK for the envelope operations it performs directly.
 resource "google_kms_crypto_key_iam_member" "app" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_service_account.app.email}"
 }

infra/terraform/logging_worm.tf
@@ -36,8 +36,11 @@ resource "google_logging_project_bucket_config" "worm_audit" {
   locked = var.worm_locked
 
   # CMEK on the log bucket (P-09): explicit, because it does not cascade.
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.cmek.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.cmek[*].id)
+    }
   }
 
   depends_on = [

infra/terraform/production_edge.tf
@@ -71,7 +71,7 @@ resource "google_cloud_run_v2_service" "api" {
     service_account = google_service_account.app.email
 
     # CMEK on the revision's own storage (P-09): it does not cascade from anywhere else.
-    encryption_key = google_kms_crypto_key.cmek.id
+    encryption_key = one(google_kms_crypto_key.cmek[*].id)
 
     scaling {
       min_instance_count = var.edge_min_instances

infra/terraform/production_edge.tftest.hcl
@@ -23,6 +23,7 @@ run "residency_defaults_are_in_country" {
   command = plan
 
   variables {
+    cmek_enabled  = true
     project_id    = "fictional-agent-project"
     enable_vpc_sc = false
   }
@@ -38,7 +39,7 @@ run "residency_defaults_are_in_country" {
   }
 
   assert {
-    condition     = google_kms_key_ring.cmek.location == local.region
+    condition     = google_kms_key_ring.cmek[0].location == local.region
     error_message = "CMEK key material must be regional and in the deployment region, never a multi-region ring."
   }
 
@@ -141,6 +142,7 @@ run "serving_edge_contract" {
   }
 
   variables {
+    cmek_enabled                = true
     project_id                  = "fictional-agent-project"
     enable_vpc_sc               = false
     production_edge_enabled     = true
@@ -168,7 +170,7 @@ run "serving_edge_contract" {
   }
 
   assert {
-    condition     = google_cloud_run_v2_service.api[0].template[0].encryption_key == google_kms_crypto_key.cmek.id
+    condition     = google_cloud_run_v2_service.api[0].template[0].encryption_key == google_kms_crypto_key.cmek[0].id
     error_message = "The revision must be bound to the regional CMEK: encryption does not cascade."
   }
 

portable-genai/contact-centre-conversations

infra/terraform/iam.tf
@@ -52,7 +52,8 @@ resource "google_project_iam_member" "app" {
 
 # The app uses the CMEK for the envelope operations it performs directly.
 resource "google_kms_crypto_key_iam_member" "app" {
-  crypto_key_id = google_kms_crypto_key.contact.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.contact[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_service_account.app.email}"
 }

infra/terraform/logging_worm.tf
@@ -35,8 +35,11 @@ resource "google_logging_project_bucket_config" "worm_audit" {
   locked = var.worm_locked
 
   # CMEK on the log bucket (P-09): explicit, because it does not cascade.
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.contact.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.contact[*].id)
+    }
   }
 
   depends_on = [

infra/terraform/production_edge.tf
@@ -80,7 +80,7 @@ resource "google_cloud_run_v2_service" "api" {
     service_account = google_service_account.app.email
 
     # CMEK on the revision's own storage (P-09): it does not cascade from anywhere else.
-    encryption_key = google_kms_crypto_key.contact.id
+    encryption_key = one(google_kms_crypto_key.contact[*].id)
 
     scaling {
       min_instance_count = var.edge_min_instances

infra/terraform/production_edge.tftest.hcl
@@ -17,6 +17,7 @@ run "residency_defaults_are_in_country" {
   command = plan
 
   variables {
+    cmek_enabled  = true
     project_id    = "fictional-contact-sg"
     enable_vpc_sc = false
   }
@@ -27,7 +28,7 @@ run "residency_defaults_are_in_country" {
   }
 
   assert {
-    condition     = google_kms_key_ring.contact.location == var.region
+    condition     = google_kms_key_ring.contact[0].location == var.region
     error_message = "CMEK key material must be regional and in the deployment region, never a multi-region ring."
   }
 
@@ -180,6 +181,7 @@ run "serving_edge_contract" {
   }
 
   variables {
+    cmek_enabled                  = true
     project_id                    = "fictional-contact-sg"
     enable_vpc_sc                 = false
     production_edge_enabled       = true
@@ -214,7 +216,7 @@ run "serving_edge_contract" {
   }
 
   assert {
-    condition     = google_cloud_run_v2_service.api[0].template[0].encryption_key == google_kms_crypto_key.contact.id
+    condition     = google_cloud_run_v2_service.api[0].template[0].encryption_key == google_kms_crypto_key.contact[0].id
     error_message = "The revision must be bound to the regional CMEK: encryption does not cascade."
   }
 